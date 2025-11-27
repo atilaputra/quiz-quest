9d4b45371ffc2c2493b9d0a132e9959416534bf9
atilaputra <haysmawarna@gmail.com>
2025-11-27 15:32:22 +0800

    Add particle.js for background animation

--- quiz.php
+++ quiz.php
@@ -182,5 +182,28 @@ $quiz_duration_minutes = 5; // User will have 5 minutes to complete the quiz
         });
 
     </script>
+        <script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+    <script>
+        particlesJS("particles-js", {
+          "particles": {
+            "number": { "value": 80, "density": { "enable": true, "value_area": 800 } },
+            "color": { "value": "#ffffff" },
+            "shape": { "type": "circle" },
+            "opacity": { "value": 0.5, "random": false },
+            "size": { "value": 3, "random": true },
+            "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 },
+            "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "bounce": false }
+          },
+          "interactivity": {
+            "detect_on": "canvas",
+            "events": {
+              "onhover": { "enable": true, "mode": "repulse" },
+              "onclick": { "enable": true, "mode": "push" },
+              "resize": true
+            }
+          },
+          "retina_detect": true
+        });
+    </script>
 </body>
 </html>
